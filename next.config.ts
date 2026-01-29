7954e41e5c2b92dff7a7e9c5a7262cfb9bd11b29
Fabian Schuller <fabianschuller2005@gmail.com>
2026-01-29 18:19:54 +0100

    stuff

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 }
 
 module.exports = nextConfig
